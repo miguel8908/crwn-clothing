a46ed440ef7286ab46960da4ba7343030742baf2
Miguel Ramirez <miguel.a8908@yahoo.com>
2020-01-16 16:08:28 -0600

    added custom button and styling

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import Header from './components/header/header-component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header-component';
 
 
 function App() {
@@ -14,7 +15,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />      
+        <Route path='/shop' component={ShopPage} />  
+        <Route path='/signin' component={SignInAndSignUpPage} />    
       </Switch>
     </div>
     );
